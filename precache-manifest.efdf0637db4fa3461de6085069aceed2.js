f829554687d1f5a396b101759f00913021c9c0c9
pawelrychlik <pawel.rychlik@rspective.pl>
2019-01-05 01:08:05 +0100

    Updates

---
+++ precache-manifest.efdf0637db4fa3461de6085069aceed2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff816526ecb5de815f9a8113a6f7698e",
+    "url": "/unstoppable/static/media/logo.ff816526.svg"
+  },
+  {
+    "revision": "7c27c83e0a312ad7b8f3",
+    "url": "/unstoppable/static/js/runtime~main.7c27c83e.js"
+  },
+  {
+    "revision": "d6819905c9f30b90b4fa",
+    "url": "/unstoppable/static/js/main.d6819905.chunk.js"
+  },
+  {
+    "revision": "8a2e5493872c9cedd577",
+    "url": "/unstoppable/static/js/1.8a2e5493.chunk.js"
+  },
+  {
+    "revision": "d6819905c9f30b90b4fa",
+    "url": "/unstoppable/static/css/main.28d84d73.chunk.css"
+  },
+  {
+    "revision": "e9489858369578d1fcd236a5584894f6",
+    "url": "/unstoppable/index.html"
+  }
+];
