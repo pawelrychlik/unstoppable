6f64f50616af1bbc96d9baf0deb4d864c6b10902
pawelrychlik <pawel.rychlik@rspective.pl>
2019-06-05 23:17:45 +0200

    Updates

---
+++ precache-manifest.a3b93f9a6845c6aaee4c7e3100d92282.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a28369a51e94f677c2d2021d466495",
+    "url": "/unstoppable/index.html"
+  },
+  {
+    "revision": "bc6dce5c766494ae9fd8",
+    "url": "/unstoppable/static/css/main.2b90221c.chunk.css"
+  },
+  {
+    "revision": "5b32397ca6b082a36794",
+    "url": "/unstoppable/static/js/2.827393a7.chunk.js"
+  },
+  {
+    "revision": "bc6dce5c766494ae9fd8",
+    "url": "/unstoppable/static/js/main.8d4fc3ce.chunk.js"
+  },
+  {
+    "revision": "be6f35f84e51682653b2",
+    "url": "/unstoppable/static/js/runtime~main.b2c47c68.js"
+  },
+  {
+    "revision": "ff816526ecb5de815f9a8113a6f7698e",
+    "url": "/unstoppable/static/media/logo.ff816526.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/unstoppable/precache-manifest.efdf0637db4fa3461de6085069aceed2.js"
+  "/unstoppable/precache-manifest.a3b93f9a6845c6aaee4c7e3100d92282.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/unstoppable/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/unstoppable/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
